c007ccbec428eb77f39342f61ad5fb40291f314a
xelad1 <xelad1@gmail.com>
2014-11-03 12:13:32 -0800

    Completed Taxonomy.

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,16 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this);
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
 };
 
+RetiredForagerBee.prototype = Object.create(Bee.prototype);
+RetiredForagerBee.prototype.forage = function(){
+  return 'I am too old, let me play cards instead';
+};
+RetiredForagerBee.prototype.gamble = function(){
+  this.treasureChest.push('treasure');
+};
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
